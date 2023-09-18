b20190e83625db0c4ab99c5907528e17f6e1f29b
I <no-name@gmail.com>
2023-09-17 20:36:42 -0400

    fix: should be 'r' for repo

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 export name=$1
 export tag=$2
-heighliner build --chain ${name} --git-ref ${tag}  -t ghcr.io/pfc-developer/heighliner
+heighliner build --chain ${name} --git-ref ${tag}  -r ghcr.io/pfc-developer/heighliner
 docker push ghcr.io/pfc-developer/heighliner/${name}:${tag}
